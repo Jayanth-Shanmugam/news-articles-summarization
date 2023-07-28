86ad95ab12b3d9db3ed752748dce2253190c11d1
Jayanth Shanmugam <mk.jayanth.s@gmail.com>
2023-07-28 17:13:08 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,5 +5,5 @@ col1, col2, col3 = st.columns([3, 3, 3])
 
 with col2:
     st.markdown("<center><h1>Welcome to the News Summaries App!</h1></center>", unsafe_allow_html = True)
-    st.image('landing_page_img.jpg')
+    st.image('assets/landing_page_img.jpg')
     st.markdown("")
