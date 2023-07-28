1fef5653288fc8c5557ecec6efdfbb9fd20be4c9
Jayanth Shanmugam <mk.jayanth.s@gmail.com>
2023-07-28 17:08:54 +0530

    Create Reuters.py

---
+++ pages/Reuters.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+
+col1, col2 = st.columns([3, 1])
+option = None
+
+with col2:
+    st.write("")
+    st.write("")
+    st.write("")
+    st.write("")
+    st.write("")
+    option = st.selectbox('', ['Crude Oil', 'Biofuel'])
+    
+with col1:
+    st.title('REUTERS')
+    st.divider()
+    if option == 'Crude Oil':
+        df = pd.read_csv('../articles/crude_oil_summary_new.csv')
+        for index, series in df.iterrows():
+            st.markdown("### [{}]({})".format(series['title'], series['links']))
+            st.markdown("*{}*".format(series['published']))
+            st.markdown('**Keywords:  {}**'.format(series['keywords']))
+            st.write(series['summaries'])
+            st.divider()
+    
+    if option == 'Biofuel':
+        df = pd.read_csv('../articles/biofuel_summary_new.csv')
+        for index, series in df.iterrows():
+            st.markdown("### [{}]({})".format(series['title'], series['links']))
+            st.markdown("*{}*".format(series['published']))
+            st.markdown('**Keywords:  {}**'.format(series['keywords']))
+            st.write(series['summaries'])
+            st.divider()
+    
